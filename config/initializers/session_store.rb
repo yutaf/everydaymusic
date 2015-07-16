b7156cd7e17f1f450e292709807fb060f56ba1a4
yutaf <yutafuji2008@gmail.com>
2015-07-16 14:03:15 +0900

    Fix initial values

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_www_session'
+Rails.application.config.session_store :cookie_store, key: '_everydaymusic_session'
