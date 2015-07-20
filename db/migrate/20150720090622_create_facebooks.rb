feb46338b160acd4ada362f5ff0ccab9ad4adcab
yutaf <yutafuji2008@gmail.com>
2015-07-20 18:10:26 +0900

    Update facebook migration

--- db/migrate/20150720090622_create_facebooks.rb
+++ db/migrate/20150720090622_create_facebooks.rb
@@ -2,9 +2,7 @@ class CreateFacebooks < ActiveRecord::Migration
   def change
     create_table :facebooks do |t|
       t.references :user, index: true, foreign_key: true
-      t.string :facebook_user_id
-      t.string :accessToken
-      t.string :salt
+      t.string :facebook_id
 
       t.timestamps null: false
     end
