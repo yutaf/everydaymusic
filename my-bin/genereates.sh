73bae8188f9888d163efa13bf5092c9864759ebb
yutaf <yutafuji2008@gmail.com>
2015-07-22 15:54:53 +0900

    Update my-bin/genereates.sh

--- my-bin/genereates.sh
+++ my-bin/genereates.sh
@@ -4,10 +4,10 @@ rails g migration CreateJoinTableUserYoutube user youtube #TODO Remenber to add 
 rails g migration CreateJoinTableFestivalUser festival user
 rails g migration CreateJoinTableArtistUser artist user
 rails g migration CreateJoinTableArtistYoutube artist youtube
-rails g scaffold user email:string name:string first_name:string last_name:string gender:string locale:string timezone:integer fetch_cnt:integer delivery_time:time interval:integer is_active:boolean
+rails g scaffold user email:string name:string first_name:string last_name:string gender:string locale:string timezone:integer fetch_cnt:integer delivery_time:time delivery_interval:time is_active:boolean
 rails g model facebook user:references facebook_user_id:string
-rails g model youtube videoId:string title:string
-rails g model festival name:string title:string
-rails g model delivery user:references delivered_at:date
 rails g model artist name:string
+rails g model festival name:string
+rails g model youtube videoId:string title:string
+rails g model delivery user:references delivered_at:date
 rails g controller logins
