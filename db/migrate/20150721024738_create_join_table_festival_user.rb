d67865984781db74dfe9e970bb0af983f8643e6d
yutaf <yutafuji2008@gmail.com>
2015-07-28 18:06:17 +0900

    Delete migration files related to festivals

--- db/migrate/20150721024738_create_join_table_festival_user.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableFestivalUser < ActiveRecord::Migration
-  def change
-    create_join_table :festivals, :users do |t|
-      # t.index [:festival_id, :user_id]
-      # t.index [:user_id, :festival_id]
-    end
-  end
-end
--- db/migrate/20150722064302_add_index_to_festivals.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToFestivals < ActiveRecord::Migration
-  def change
-    add_index :festivals, :name, unique: true
-  end
-end
