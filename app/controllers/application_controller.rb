eb989f2591b0c994cc27924f6c45f5f00d20d25d
yutaf <yutafuji2008@gmail.com>
2015-08-07 10:31:07 +0900

    Add is_logged_in filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  # is this right manner?
+  def is_logged_in?
+    authcookie = cookies[:auth]
+    if authcookie.blank?
+      return false
+    end
+    redis = Redis.new(host: ENV['REDIS_HOST']);
+    user_id = redis.hget('auths', authcookie);
+    if user_id.blank?
+      return false
+    end
+    authsecret = redis.hget("user:#{user_id}", 'auth')
+    if authcookie != authsecret
+      return false
+    end
+
+    return true
+  end
 end
