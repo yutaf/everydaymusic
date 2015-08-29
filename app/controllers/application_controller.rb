96fcdbf4f664fc76ad48385da42a404a57dfb7e6
yutaf <yutafuji2008@gmail.com>
2015-08-29 14:47:05 +0900

    Fix login check & Add I18n implemention

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :before_action_login
 
   private
-  # is this right manner?
-  def is_logged_in?
+  def before_action_login
+    # Check logged in
     authcookie = cookies[:auth]
     if authcookie.blank?
-      return false
+      redirect_to '/login'
     end
     redis = Redis.new(host: ENV['REDIS_HOST']);
-    user_id = redis.hget('auths', authcookie);
-    if user_id.blank?
-      return false
+    @user_id = redis.hget('auths', authcookie);
+    if @user_id.blank?
+      redirect_to '/login'
     end
-    authsecret = redis.hget("user:#{user_id}", 'auth')
+    authsecret = redis.hget("user:#{@user_id}", 'auth')
     if authcookie != authsecret
-      return false
+      redirect_to '/login'
     end
 
-    return true
+    # Set locale value to session
+    if session[:locale].nil?
+      user = User.find_by(@user_id)
+      session[:locale] = user[:locale][0,2]
+    end
+    case session[:locale]
+      when 'ja', 'en'
+        I18n.locale = session[:locale]
+      else
+        ;
+    end
   end
 end
