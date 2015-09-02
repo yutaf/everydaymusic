c547268dec471f149a0de9e0971b5370311ee2fc
yutaf <yutafuji2008@gmail.com>
2015-09-02 20:48:27 +0900

    Add AccountsController.set_user

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,4 +3,9 @@ class AccountsController < ApplicationController
     @user = User.find_by(@user_id)
     render json: @user
   end
+
+  private
+    def set_user
+      @user = User.find_by(@user_id)
+    end
 end
