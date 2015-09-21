4a4f4964bd2601d9bc2d6c02056fabd7f093c30f
yutaf <yutafuji2008@gmail.com>
2015-09-21 16:38:33 +0900

    Implement transaction

--- lib/batches/insert_into_deliveries.rb
+++ lib/batches/insert_into_deliveries.rb
@@ -16,84 +16,84 @@ class InsertIntoDeliveries
 
     artists_to_import = []
 
-    #TODO transaction
-    
-    artist_records = Artist.pluck(:name)
+    # Begin transaction
+    begin
+      ActiveRecord::Base.transaction do
+        artist_records = Artist.pluck(:name)
+        users = User.includes(:artists).all.select(:id)
+        users.each do |user|
 
-    users = User.includes(:artists).all.select(:id)
-    users.each do |user|
+          if user.artists.count == 0
+            next
+          end
 
-      if user.artists.count == 0
-        next
-      end
+          # Define artist being used as a search keyword
+          artist = user.artists.to_a.sample(1)[0][:name]
 
-      # Define artist being used as a search keyword
-      artist = user.artists.to_a.sample(1)[0][:name]
+          # search related artists at a rate of 1 / 5
+          if 1 == rand(5)
+            spotify_artists = RSpotify::Artist.search(artist)
+            if spotify_artists.instance_of?(Array) && spotify_artists.count > 0
+              spotify_related_artists = spotify_artists.first.related_artists
+              if spotify_related_artists.instance_of?(Array) && spotify_related_artists.count > 0
+                # Sort array randomly
+                spotify_related_artists.shuffle!
 
-      # search related artists at a rate of 1 / 5
-      if 1 == rand(5)
-        spotify_artists = RSpotify::Artist.search(artist)
-        if spotify_artists.instance_of?(Array) && spotify_artists.count > 0
-          spotify_related_artists = spotify_artists.first.related_artists
-          if spotify_related_artists.instance_of?(Array) && spotify_related_artists.count > 0
-            # Sort array randomly
-            spotify_related_artists.shuffle!
-
-            spotify_related_artists.each do |a|
-              # check duplication of artists
-              if artist_records.include? a.name
-                next
+                spotify_related_artists.each do |a|
+                  # check duplication of artists
+                  if artist_records.include? a.name
+                    next
+                  end
+                  # Update artist
+                  artist = a.name
+                  # Add artist to import array
+                  artist_model = Artist.new(name: artist)
+                  artists_to_import << artist_model
+                  break
+                end
               end
-              # Update artist
-              artist = a.name
-              # Add artist to import array
-              artist_model = Artist.new(name: artist)
-              artists_to_import << artist_model
-              break
             end
           end
+
+          search_query = artist
+          # Add random words at a rate of 1 / 5
+          if 1 == rand(5)
+            search_query += ' ' + random_words_for_search.sample(1)[0]
+          end
+
+          begin
+            search_response = client.execute!(
+                :api_method => youtube.search.list,
+                :parameters => {
+                    # part: 'snippet',
+                    part: 'id',
+                    type: 'video',
+                    q: search_query,
+                    videoEmbeddable: 'true',
+                    maxResults: 5
+                }
+            )
+
+            if ! search_response.data.items.instance_of?(Array) && search_response.data.items == 0
+              # TODO log
+              next
+            end
+            #TODO existence check search_response.data.items.sample(1)[0].id.videoId
+            # pp search_response.data.items.sample(1)[0].id.videoId
+          rescue Google::APIClient::TransmissionError => e
+            puts e.result.body
+          end
+        end
+
+        if artists_to_import.count > 0
+          # Bulk insert into artists
+          # TODO validation
+          Artist.import artists_to_import
         end
       end
-
-      search_query = artist
-      # Add random words at a rate of 1 / 5
-      if 1 == rand(5)
-        search_query += ' ' + random_words_for_search.sample(1)[0]
-      end
-
-      begin
-        search_response = client.execute!(
-            :api_method => youtube.search.list,
-            :parameters => {
-                # part: 'snippet',
-                part: 'id',
-                type: 'video',
-                q: search_query,
-                videoEmbeddable: 'true',
-                maxResults: 5
-            }
-        )
-
-        if ! search_response.data.items.instance_of?(Array) && search_response.data.items == 0
-          # TODO log
-          next
-        end
-
-        # pp search_response.data.items.sample(1)[0]
-
-        #TODO existence check search_response.data.items.sample(1)[0].id.videoId
-        # pp search_response.data.items.sample(1)[0].id.videoId
-
-
-      rescue Google::APIClient::TransmissionError => e
-        puts e.result.body
-      end
-    end
-
-    if artists_to_import.count > 0
-      # Bulk insert into artists
-      # TODO validation
-      Artist.import artists_to_import
+    rescue => e
+      #TODO log
+      pp e
     end
   end
 
