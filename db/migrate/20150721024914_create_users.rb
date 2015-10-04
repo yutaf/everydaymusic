a6600a2119cfdd9da84feaef65cf97ff93d281bb
yutaf <yutafuji2008@gmail.com>
2015-10-04 17:32:19 +0900

    Update column definitions of users

--- db/migrate/20150721024914_create_users.rb
+++ db/migrate/20150721024914_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :locale
-      t.integer :timezone, limit: 1
-      t.time :delivery_time
+      t.string :email, null: false, default: ''
+      t.string :locale, null: false, default: ENV['DEFAULT_LOCALE']
+      t.integer :timezone, limit: 1, null: false, default: 0
+      t.time :delivery_time, null: false, default: ENV['DEFAULT_DELIVERY_TIME']
       t.boolean :is_active, null: false, default: 1
 
       t.timestamps null: false
