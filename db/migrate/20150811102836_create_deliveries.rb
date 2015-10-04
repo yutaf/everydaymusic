0d5ff978361a93a6bb77d72ca4ed1b22f8ce1835
yutaf <yutafuji2008@gmail.com>
2015-10-04 17:35:21 +0900

    Update table definition of deliveries

--- db/migrate/20150811102836_create_deliveries.rb
+++ db/migrate/20150811102836_create_deliveries.rb
@@ -1,9 +1,8 @@
 class CreateDeliveries < ActiveRecord::Migration
   def change
     create_table :deliveries do |t|
-      t.datetime :date
-      t.references :user, index: true, foreign_key: true
-      t.string :video_id, index: true
+      t.references :user, null: false, index: true, foreign_key: true
+      t.string :video_id, null: false, index: true
       t.boolean :is_delivered, null: false, default: 0
 
       t.timestamps null: false
