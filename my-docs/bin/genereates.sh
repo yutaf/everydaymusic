3eb72c5006b58eec18f62d38c5c511ad6e0a841b
yutaf <yutafuji2008@gmail.com>
2015-10-04 17:31:10 +0900

    Add delivery_date model & Remove delivery.date

--- my-docs/bin/genereates.sh
+++ my-docs/bin/genereates.sh
@@ -4,7 +4,8 @@ rails g migration CreateJoinTableArtistUser artist user
 rails g scaffold user email:string locale:string timezone:integer delivery_time:time is_active:boolean
 rails g model facebook user:references facebook_user_id:string
 rails g model artist name:string
-rails g model delivery date:datetime user:references youtube:references is_delivered:boolean
+rails g model delivery user:references video_id:references is_delivered:boolean
+rails g model delivery_date delivery:references date:datetime
 
 rails g controller logins
 rails g controller lists
