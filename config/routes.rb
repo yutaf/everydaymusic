8b2488503188166f8a1a8600a7b1e1b6551df401
yutaf <yutafuji2008@gmail.com>
2015-10-23 15:30:32 +0900

    Add logout to routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resource :account, format: false, only: [:edit, :show, :update, :destroy]
 
   resource :login, format: false, only: [:show]
+  resource :logout, format: false, only: [:show]
   resource :list, format: false, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
