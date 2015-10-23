617a36b25991875ccb388c601c51fd5b2c2f446f
yutaf <yutafuji2008@gmail.com>
2015-10-23 15:29:57 +0900

    Create logouts controller

---
+++ app/controllers/logouts_controller.rb
@@ -0,0 +1,12 @@
+class LogoutsController < ApplicationController
+  def show
+    newauthsecret = ('a'..'z').to_a.shuffle[0,8].join
+    oldauthsecret = @redis.hget("user:#{@user_id}", 'auth')
+    @redis.hset("user:#{@user_id}", 'auth', newauthsecret)
+    @redis.hset('auths', newauthsecret, @user_id)
+    @redis.hdel('auths', oldauthsecret)
+
+    redirect_to '/login'
+    return
+  end
+end
---
+++ test/controllers/logouts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LogoutsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
