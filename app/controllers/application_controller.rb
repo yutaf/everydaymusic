b864678cc1482c601dde98db5c8e6bb9ee1fbcb3
yutaf <yutafuji2008@gmail.com>
2015-10-24 13:45:39 +0900

    Replace 'initialize' with 'init'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :initialize
+  before_action :init
 
   private
-  def initialize
+  def init
     if ! is_logged_in?
       redirect_to '/login'
       return
