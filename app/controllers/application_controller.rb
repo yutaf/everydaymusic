25503ede2911cbb200c6eff2080aaabc993e86f8
yutaf <yutafuji2008@gmail.com>
2015-10-24 12:32:42 +0900

    Add ApplicationController::is_logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,4 +36,21 @@ class ApplicationController < ActionController::Base
         ;
     end
   end
+
+  def is_logged_in?
+    authcookie = cookies[:auth]
+    if authcookie.blank?
+      return false
+    end
+    @redis = Redis.new(host: ENV['REDIS_HOST'])
+    @user_id = @redis.hget('auths', authcookie)
+    if @user_id.blank?
+      return false
+    end
+    authsecret = @redis.hget("user:#{@user_id}", 'auth')
+    if authcookie != authsecret
+      return false
+    end
+    return true
+  end
 end
