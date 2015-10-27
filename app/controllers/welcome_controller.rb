971d0271ee53e34130208587d7b077d2ccfc31da
yutaf <yutafuji2008@gmail.com>
2015-10-27 16:36:09 +0900

    Fix behavior in welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,7 @@ class WelcomeController < ApplicationController
   skip_before_action :init
   def index
     if is_logged_in?
-      set_locale
-      @header = 'header'
-      @is_logged_in = true
-      #TODO render list link
-    else
-      @header = 'header_plain'
-      @is_logged_in = false
+      redirect_to '/list'
     end
   end
 end
