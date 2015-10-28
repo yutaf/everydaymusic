4f434bb657d860d562729e75ab3175b90f0dcf08
yutaf <yutafuji2008@gmail.com>
2015-10-28 18:17:52 +0900

    Implement  redis.multi

--- app/controllers/logout_controller.rb
+++ app/controllers/logout_controller.rb
@@ -2,9 +2,11 @@ class LogoutController < ApplicationController
   def index
     newauthsecret = ('a'..'z').to_a.shuffle[0,8].join
     oldauthsecret = @redis.hget("user:#{@user_id}", 'auth')
-    @redis.hset("user:#{@user_id}", 'auth', newauthsecret)
-    @redis.hset('auths', newauthsecret, @user_id)
-    @redis.hdel('auths', oldauthsecret)
+    @redis.multi do |multi|
+      multi.hset("user:#{@user_id}", 'auth', newauthsecret)
+      multi.hset('auths', newauthsecret, @user_id)
+      multi.hdel('auths', oldauthsecret)
+    end
 
     redirect_to '/'
   end
