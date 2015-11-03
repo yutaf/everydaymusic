00017458d3c5d3874d735504a38b802d44438303
yutaf <yutafuji2008@gmail.com>
2015-11-03 16:28:13 +0900

    Update config.action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,18 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+      user_name: ENV['SMTP_USER_NAME'],
+      password: ENV['SMTP_PASSWORD'],
+      domain: '192.168.11.92.xip.io',
+      address: 'smtp.sendgrid.net',
+      port: 587,
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'www.example.com' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
