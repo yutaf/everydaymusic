b1e0229a7127bc022a009f4b6b9d0f717358a3f8
yutaf <yutafuji2008@gmail.com>
2015-11-03 16:26:34 +0900

    Set sidekiq as job adapter

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Everydaymusic
     config.active_record.raise_in_transactional_callbacks = true
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.active_job.queue_adapter = :sidekiq
   end
 end
