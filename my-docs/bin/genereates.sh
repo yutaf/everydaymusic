9eb2390bb448958adc7472d79ba443ac49d5971b
yutaf <yutafuji2008@gmail.com>
2015-12-09 19:14:54 +0900

    Remove generate delivery_date

--- my-docs/bin/genereates.sh
+++ my-docs/bin/genereates.sh
@@ -4,8 +4,7 @@ rails g migration CreateJoinTableArtistUser artist user
 rails g scaffold user email:string locale:string timezone:integer delivery_time:time is_active:boolean
 rails g model facebook user:references facebook_user_id:string
 rails g model artist name:string
-rails g model delivery user:references video_id:references is_delivered:boolean
-rails g model delivery_date delivery:references date:datetime
+rails g model delivery user:references video_id:references date:datetime is_delivered:boolean
 
 rails g controller accounts
 rails g controller list index
