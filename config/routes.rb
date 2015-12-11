869acd5ed0a0ae63460be9af52ec04fe63bc47be
yutaf <yutafuji2008@gmail.com>
2015-12-11 18:46:59 +0900

    Add sidekiq/web to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resource :account, format: false, only: [:edit, :show, :update, :destroy]
   get 'list' => 'list#index', format: false
   get 'logout' => 'logout#index', format: false
+  # sidekiq/web
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   root 'welcome#index'
 end
