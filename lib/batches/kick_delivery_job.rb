ef564cdc909f3d2da940986858b136fbffac6a63
yutaf <yutafuji2008@gmail.com>
2015-12-11 19:28:03 +0900

    Clear scheduled delivery ques before execute DeliveryJob

--- lib/batches/kick_delivery_job.rb
+++ lib/batches/kick_delivery_job.rb
@@ -1,5 +1,12 @@
 class KickDeliveryJob
   def self.execute
+    require 'sidekiq/api'
+
+    # clear scheduled delivery ques
+    r = Sidekiq::ScheduledSet.new
+    jobs = r.select {|retri| retri.queue == 'delivery' }
+    jobs.each(&:delete)
+
     DeliveryJob.perform_later
   end
   KickDeliveryJob.execute
