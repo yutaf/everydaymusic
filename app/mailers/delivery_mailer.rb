e27b8a106cbc87febb15f996d0e7d54d22aa5911
yutaf <yutafuji2008@gmail.com>
2015-12-12 16:08:29 +0900

    Create app/mailers/delivery_mailer.rb

---
+++ app/mailers/delivery_mailer.rb
@@ -0,0 +1,15 @@
+class DeliveryMailer < ApplicationMailer
+
+  default from: 'info@everydaymusic.net',
+          subject: 'everydaymusic'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.delivery_mailer.sendmail.subject
+  #
+  def sendmail(email, video_id)
+    mail to: email,
+         body: "https://www.youtube.com/watch?v=#{video_id}"
+  end
+end
