a2c0f77b44874df1b93c64abfa03e2147c2b40b0
yutaf <yutafuji2008@gmail.com>
2015-12-15 22:32:57 +0900

    Switch facebook app id by environment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,5 +4,10 @@ class WelcomeController < ApplicationController
     if is_logged_in?
       redirect_to '/list'
     end
+
+    @facebook_app_id = ENV['FACEBOOK_APP_ID']
+    if request.env['HTTP_HOST'].end_with?('xip.io')
+      @facebook_app_id = ENV['FACEBOOK_APP_ID_DEVELOPMENT']
+    end
   end
 end
