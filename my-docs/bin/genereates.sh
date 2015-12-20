80c7419a80b9056e593c2e3141a2599ee29867e1
yutaf <yutafuji2008@gmail.com>
2015-12-20 20:08:14 +0900

    Replace 'generate scaffold user' with 'generate model user'

--- my-docs/bin/genereates.sh
+++ my-docs/bin/genereates.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rails g migration CreateJoinTableArtistUser artist user
-rails g scaffold user email:string locale:string timezone:integer delivery_time:time is_active:boolean
+rails g model user email:string locale:string timezone:integer delivery_time:time is_active:boolean
 rails g model facebook user:references facebook_user_id:string
 rails g model artist name:string
 rails g model delivery user:references video_id:references date:datetime is_delivered:boolean
