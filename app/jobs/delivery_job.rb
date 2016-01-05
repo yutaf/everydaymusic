348ce427077130fa4bcdd3485e89fa1eb8ed65b4
yutaf <yutafuji2008@gmail.com>
2016-01-05 21:32:37 +0900

    Clear delivery job in delivery_job script itself

--- app/jobs/delivery_job.rb
+++ app/jobs/delivery_job.rb
@@ -2,6 +2,13 @@ class DeliveryJob < ActiveJob::Base
   queue_as :delivery
 
   def perform
+
+    require 'sidekiq/api'
+
+    # clear scheduled delivery ques
+    r = Sidekiq::ScheduledSet.new
+    jobs = r.select {|retri| retri.queue == 'delivery' }
+    jobs.each(&:delete)
     # Set next schedule
     self.class.set(wait: 1.hour).perform_later
 
