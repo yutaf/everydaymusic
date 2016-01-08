363ba29dd6c4df2e99c7dbc0ca2d5313184a81b2
yutaf <yutafuji2008@gmail.com>
2016-01-08 17:27:51 +0900

    Delete comments

--- lib/batches/kick_delivery_job.rb
+++ lib/batches/kick_delivery_job.rb
@@ -1,14 +1,5 @@
 class KickDeliveryJob
   def self.execute
-=begin
-    require 'sidekiq/api'
-
-    # clear scheduled delivery ques
-    r = Sidekiq::ScheduledSet.new
-    jobs = r.select {|retri| retri.queue == 'delivery' }
-    jobs.each(&:delete)
-=end
-
     DeliveryJob.perform_later
   end
   KickDeliveryJob.execute
