d1fb6d6a18a2acdca18ccf4029b553b9229de08b
yutaf <yutafuji2008@gmail.com>
2016-01-08 19:01:45 +0900

    Create unsubscribe_controller

--- app/controllers/unsubscribe_controller.rb
+++ app/controllers/unsubscribe_controller.rb
@@ -1,4 +1,18 @@
 class UnsubscribeController < ApplicationController
+  skip_before_action :init
   def index
+    unsubscribe_key = params[:key]
+    if unsubscribe_key.blank?
+      return
+    end
+    redis = Redis.new(host: ENV['REDIS_HOST'])
+    user_id = redis.hget('unsubscribe_keys', unsubscribe_key)
+    if user_id.blank?
+      return
+    end
+    user = User.find(user_id)
+    if user.update(is_active: false)
+      redis.hdel('unsubscribe_keys', unsubscribe_key)
+    end
   end
 end
