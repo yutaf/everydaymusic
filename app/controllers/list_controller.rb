61bc6ce1d2b626b1aee75a036b4067c6a14bebd2
yutaf <yutafuji2008@gmail.com>
2016-01-29 20:31:19 +0900

    Add no_artist, no_artist views to list controller

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -2,15 +2,26 @@ class ListController < ApplicationController
   def index
     # TODO reduce database access count
     @deliveries = Delivery.where(user: @user_id, is_delivered: true).order(id: :desc)
-    user_redis = @redis.hgetall("user:#{@user_id}")
-    timezone = user_redis['timezone'].to_i
-    timezone_abs = timezone.abs
-    operator = '-'
-    if timezone < 0
-        operator = '+'
+    if @deliveries.size == 0
+      user = User.select(:id, :timezone, :delivery_time).find(@user_id)
+      artist_ids = user.artist_ids
+      # render json: artist_ids
+      if artist_ids.size == 0
+        render action: :no_artist
+      else
+        timezone = user['timezone'].to_i
+        timezone_abs = timezone.abs
+        operator = '-'
+        if timezone < 0
+          operator = '+'
+        end
+        hour = "%02d"%[timezone_abs]
+
+        utc_date = user['delivery_time'].strftime("%F %T")
+        time = Time.zone.parse("#{utc_date} #{operator}#{hour}00")
+        @delivery_time_local = time.strftime('%H:%M')
+        render action: :welcome
+      end
     end
-    hour = "%02d"%[timezone_abs]
-    time = Time.zone.parse("2007-02-10 #{user_redis['delivery_time']} #{operator}#{hour}00")
-    @delivery_time_local = time.strftime('%H:%M')
   end
 end
