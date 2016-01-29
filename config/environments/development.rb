1faba6d1eae519cc4d44039f91d6930e61e98cc6
yutaf <yutafuji2008@gmail.com>
2016-01-29 16:31:21 +0900

    Replace config.action_mailer.default_url_options with 192.168.11.92.xip.io

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
       authentication: :plain,
       enable_starttls_auto: true
   }
-  config.action_mailer.default_url_options = { host: 'www.example.com' }
+  config.action_mailer.default_url_options = { host: '192.168.11.92.xip.io' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
