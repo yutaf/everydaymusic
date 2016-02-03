a71a6dd3c6492ce98e8ee04277834c002623b74a
yutaf <yutafuji2008@gmail.com>
2016-02-03 17:47:28 +0900

    Check delivery record existence

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,7 +1,16 @@
 class DeliveriesController < ApplicationController
   skip_before_action :init
   def show
+    @delivery = Delivery.find_by(id: params[:id])
+    if @delivery.blank?
+      #TODO Render error page
+      location = '/'
+      if is_logged_in?
+        location = '/list'
+      end
+      redirect_to location
+      return
+    end
     @is_logged_in = is_logged_in?
-    @delivery = Delivery.find(params[:id])
   end
 end
