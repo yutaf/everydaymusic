63e2554597c44a1c6d583c90cdc21bc1178c91e2
yutaf <yutafuji2008@gmail.com>
2016-03-02 18:16:16 +0900

    Implement like artist checkbox on/off

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -11,8 +11,12 @@ class DeliveriesController < ApplicationController
       redirect_to location
       return
     end
+
     @is_logged_in = is_logged_in?
     @meta_title = "#{@delivery.title} | #{MY_APP['meta']['site_name']}"
     @og_image = "https://i.ytimg.com/vi/#{@delivery.video_id}/hqdefault.jpg"
+
+    user = User.find(@user_id)
+    @checked = user.artists.exists?(@delivery.artist_id)
   end
 end
