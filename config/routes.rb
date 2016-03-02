5233c8485c89f284bdafe1896003efa14c2fce2a
yutaf <yutafuji2008@gmail.com>
2016-03-02 19:26:33 +0900

    Add account/add_artist, account/delete_artist to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resource :account, format: false, only: [:edit, :show, :update]
+  post 'account/add_artist' => 'accounts#add_artist', format: false
+  post 'account/delete_artist' => 'accounts#delete_artist', format: false
   get 'list' => 'list#index', format: false
   get 'logout' => 'logout#index', format: false
   get 'policies/privacy', format: false
