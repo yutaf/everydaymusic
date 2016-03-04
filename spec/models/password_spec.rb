e774b26d394ffb6f71fcf5d4eef2609a54f495d0
yutaf <yutafuji2008@gmail.com>
2016-03-04 16:56:35 +0900

    Update spec/models/password_spec.rb

--- spec/models/password_spec.rb
+++ spec/models/password_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Password, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with password and password_confirmation" do
+    password = Password.new(password: "foobar", password_confirmation: "foobar")
+    expect(password).to be_valid
+  end
 end
