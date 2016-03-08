a009a8343d2da599f79a943ebcce1e3a65a5a7a5
yutaf <yutafuji2008@gmail.com>
2016-03-08 20:40:22 +0930

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'unsubscribe/index', format: false
   get 'unsubscribe' => 'unsubscribe#index', format: false
   get 'deliveries/:id' => 'deliveries#show', format: false
-  post 'welcome' => 'welcome#create_user', format: false
+  post 'welcome' => 'welcome#signup', format: false
 
   # sidekiq/web
   require 'sidekiq/web'
