7e34b6c7fa151f70d71a88fb8226287d2e67b6f0
yutaf <yutafuji2008@gmail.com>
2016-03-08 17:34:45 +0930

    WIP:Improve saving user model on sign up

--- app/assets/javascripts/welcome.coffee
+++ app/assets/javascripts/welcome.coffee
@@ -1,3 +1,8 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on 'submit', 'form', (e) ->
+  offset = new Date().getTimezoneOffset()
+  timezone = Math.floor(-(offset / 60))
+  $(this).append('<input type="hidden" name="user[timezone]" value="'+timezone+'">')
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,15 +12,33 @@ class WelcomeController < ApplicationController
   end
 
   def create_user
-    @user = User.new(user_params)
+    user_params_requested = user_params
+
+    locale = ENV['DEFAULT_LOCALE']
+    if request.headers['HTTP_ACCEPT_LANGUAGE'].present?
+      locale = request.headers['HTTP_ACCEPT_LANGUAGE'].scan(/\A[a-z]{2}/).first
+    end
+
+    # delivery_time
+    timezone = user_params_requested[:timezone].to_i
+    timezone_abs = timezone.abs
+    operator = '+'
+    if timezone < 0
+      operator = '-'
+    end
+    diff_hour = "%02d"%[timezone_abs]
+    utc_date = Time.zone.parse("2015-03-08 #{ENV['DEFAULT_DELIVERY_TIME']} #{operator}#{diff_hour}00")
+    delivery_time = utc_date.strftime("%T")
+
+    user_params_additional = {locale: locale, delivery_time: delivery_time, is_active: true}
+    user_params_new = user_params_requested.merge(user_params_additional)
+    @user = User.new(user_params_new)
     @user.build_password(password_params)
 
     if @user.save
       #TODO sign up
-      # @user.password << @password
-      # @user.save
     else
-      # Remove "is invalid" message by validates_associated
+      # Remove "is invalid" message by validates_associated of User model
       @user.errors.delete :password
 
       @error_messages = @user.errors.full_messages.concat(@user.password.errors.full_messages)
@@ -33,7 +51,7 @@ class WelcomeController < ApplicationController
     @facebook_app_id = ENV['FACEBOOK_APP_ID']
   end
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :timezone)
   end
   def password_params
     params.require(:password).permit(:password, :password_confirmation)
