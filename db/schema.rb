db821155f5ac44cd289a5b3143cdab8f8a2bd934
yutaf <yutafuji2008@gmail.com>
2016-04-17 18:53:05 +0900

    Update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160304065312) do
+ActiveRecord::Schema.define(version: 20160417093554) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160304065312) do
     t.integer "artist_id", limit: 4, null: false
     t.integer "user_id",   limit: 4, null: false
   end
+
+  add_index "artists_users", ["artist_id", "user_id"], name: "index_artists_users_on_artist_id_and_user_id", unique: true, using: :btree
 
   create_table "deliveries", force: :cascade do |t|
     t.integer  "user_id",      limit: 4,                   null: false
