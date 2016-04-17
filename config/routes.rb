d757b854956cf3e9016c079ba17e6fffca0d4f7a
yutaf <yutafuji2008@gmail.com>
2016-04-17 18:53:20 +0900

    Add account/artists to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resource :account, format: false, only: [:edit, :show, :update]
   post 'account/add_artist' => 'accounts#add_artist', format: false
   post 'account/delete_artist' => 'accounts#delete_artist', format: false
+  get 'account/artists' => 'accounts#edit_artists', format: false
+  post 'account/artists' => 'accounts#update_artists', format: false
   get 'signup/artists' => 'accounts#edit_artists', format: false
   post 'signup/artists' => 'accounts#update_artists', format: false
   get 'list' => 'list#index', format: false
