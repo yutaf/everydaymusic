07b2c80600fcf6c6b550ee76ba9689f224965c75
yutaf <yutafuji2008@gmail.com>
2016-04-25 15:51:44 +0900

    Fix application logger setting

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module Everydaymusic
     config.autoload_paths += Dir["#{config.root}/app/jobs/**/"]
     config.active_job.queue_adapter = :sidekiq
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class='field_with_errors'>#{html_tag}</span>".html_safe }
+    config.logger = Logger.new("log/#{Rails.env}.log", 5, 10 * 1024 * 1024)
   end
 end
